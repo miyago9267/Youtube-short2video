27d880b0660a66abc8eaf99292b17d8676ad6812
miyago9267 <miyagomizugen@gmail.com>
2023-05-04 21:55:04 +0800

    add redirct function

--- content.js
+++ content.js
@@ -1,15 +1,7 @@
 function short_to_video() {
     let currentUrl = window.location.href;
     newUrl = currentUrl.replace('shorts/', 'watch?v=');
-    window.location.href = newUrl;
-    location.replace(newUrl);
-
-    setTimeout(function() {
-        location.replace(newUrl);
-    }, 1000);
+    window.location.assign(newUrl);
 }
 
-
-
-// short_to_video();
-// location.reload();
+short_to_video();
